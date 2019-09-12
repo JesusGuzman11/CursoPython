bc5d37dc1128fc0dda9d998a8b41dc2242df435e
Jesus Guzman <jesusguzman.0311@gmail.com>
2019-09-12 11:53:22 -0500

    Ejemplo para dibujar un cuadrado

---
+++ ejemplo_inicial.py
@@ -0,0 +1,17 @@
+# ------------------------------- Importaciones ------------------------------ #
+import turtle;
+# ---------------------------------------------------------------------------- #
+window = turtle.Screen()
+jesus = turtle.Turtle()
+jesus.forward(50)
+jesus.left(90)
+jesus.forward(50)
+jesus.left(90)
+jesus.forward(50)
+jesus.left(90)
+jesus.forward(50)
+jesus.left(90)
+
+turtle.mainloop()
+
+
