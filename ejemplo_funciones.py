3b5b49f6cb9f451579677a856f3d777fc7bad83d
Jesus Guzman <jesusguzman.0311@gmail.com>
2019-09-13 11:25:59 -0500

    Ejemplo Funciones

---
+++ ejemplo_funciones.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import turtle
+
+def main():
+    window = turtle.Screen()
+    jesus = turtle.Turtle()
+
+    make_square(jesus)
+
+    turtle.mainloop()
+
+def make_square(jesus):
+    length = int(input('Tamaño de cuadrado: '))
+
+    for i in range(4):
+        make_line_and_turn(jesus, length)
+
+def make_line_and_turn(jesus, length):
+    jesus.forward(length)
+    jesus.left(90)
+
+if __name__ == '__main__':
+    main()
